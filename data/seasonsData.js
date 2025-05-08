784a2bc26cbfd33c63998544aff92ec7c511fb9a
Ruutuli <rudhuli@gmail.com>
2025-05-08 14:15:30 -0400

    chore: Update .gitignore to streamline ignored files

---
+++ data/seasonsData.js
@@ -0,0 +1,544 @@
+module.exports = {
+    Rudania: {
+      village: "Rudania",
+      seasons: {
+        Winter: {
+          Temperature: [
+            "24°F / -4°C - Cold",
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heavy Rain",
+            "Light Rain",
+            "Light Snow",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Snow",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Flood",
+            "Meteor Shower",
+            "Rock Slide",
+            "Blight Rain"
+          ]
+        },
+        Spring: {
+          Temperature: [
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect",
+            "82°F / 28°C - Warm",
+            "89°F / 32°C - Hot",
+            "97°F / 36°C - Scorching"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cinder Storm",
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heavy Rain",
+            "Light Rain",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Drought",
+            "Fairy Circle",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Muggy",
+            "Blight Rain"
+          ]
+        },
+        Summer: {
+          Temperature: [
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect",
+            "82°F / 28°C - Warm",
+            "89°F / 32°C - Hot",
+            "97°F / 36°C - Scorching",
+            "100°F / 38°C - Heat Wave"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cinder Storm",
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heat Lightning",
+            "Heavy Rain",
+            "Light Rain",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Drought",
+            "Fairy Circle",
+            "Flood",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Muggy",
+            "Rock Slide",
+            "Blight Rain"
+          ]
+        },
+        Autumn: {
+          Temperature: [
+            "24°F / -4°C - Cold",
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cinder Storm",
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heat Lightning",
+            "Heavy Rain",
+            "Light Rain",
+            "Light Snow",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Drought",
+            "Fairy Circle",
+            "Flower Bloom",
+            "Meteor Shower",
+            "Muggy",
+            "Rock Slide",
+            "Blight Rain"
+          ]
+        }
+      }
+    },
+    Vhintl: {
+      village: "Vhintl",
+      seasons: {
+        Winter: {
+          Temperature: [
+            "24°F / -4°C - Cold",
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heavy Rain",
+            "Light Rain",
+            "Light Snow",
+            "Partly cloudy",
+            "Rain",
+            "Sleet",
+            "Snow",
+            "Sunny",
+            "Thundersnow",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Fairy Circle",
+            "Meteor Shower",
+            "Blight Rain"
+          ]
+        },
+        Spring: {
+          Temperature: [
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect",
+            "82°F / 28°C - Warm",
+            "89°F / 32°C - Hot"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heat Lightning",
+            "Heavy Rain",
+            "Light Rain",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Snow",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Fairy Circle",
+            "Flood",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Blight Rain"
+          ]
+        },
+        Summer: {
+          Temperature: [
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect",
+            "82°F / 28°C - Warm",
+            "89°F / 32°C - Hot",
+            "97°F / 36°C - Scorching"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heat Lightning",
+            "Heavy Rain",
+            "Light Rain",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Fairy Circle",
+            "Flood",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Muggy",
+            "Blight Rain"
+          ]
+        },
+        Autumn: {
+          Temperature: [
+            "24°F / -4°C - Cold",
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heavy Rain",
+            "Light Rain",
+            "Light Snow",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Sunny",
+            "Thundersnow",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Fairy Circle",
+            "Flood",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Muggy",
+            "Blight Rain"
+          ]
+        }
+      }
+    },
+    Inariko: {
+      village: "Inariko",
+      seasons: {
+        Winter: {
+          Temperature: [
+            "0°F / -18°C - Frigid",
+            "8°F / -14°C - Freezing",
+            "24°F / -4°C - Cold",
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Blizzard",
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heavy Rain",
+            "Heavy Snow",
+            "Light Rain",
+            "Light Snow",
+            "Partly cloudy",
+            "Rain",
+            "Sleet",
+            "Snow",
+            "Sunny",
+            "Thundersnow",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Avalanche",
+            "Meteor Shower",
+            "Blight Rain"
+          ]
+        },
+        Spring: {
+          Temperature: [
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect",
+            "82°F / 28°C - Warm"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heavy Rain",
+            "Light Rain",
+            "Light Snow",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Snow",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Fairy Circle",
+            "Flood",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Blight Rain"
+          ]
+        },
+        Summer: {
+          Temperature: [
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect",
+            "82°F / 28°C - Warm",
+            "89°F / 32°C - Hot",
+            "97°F / 36°C - Scorching"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heat Lightning",
+            "Heavy Rain",
+            "Light Rain",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sun Shower",
+            "Sunny",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Fairy Circle",
+            "Flood",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Muggy",
+            "Blight Rain"
+          ]
+        },
+        Autumn: {
+          Temperature: [
+            "24°F / -4°C - Cold",
+            "36°F / 2°C - Chilly",
+            "44°F / 6°C - Brisk",
+            "52°F / 11°C - Cool",
+            "61°F / 16°C - Mild",
+            "72°F / 22°C - Perfect"
+          ],
+          Wind: [
+            "< 2(km/h) // Calm",
+            "2 - 12(km/h) // Breeze",
+            "13 - 30(km/h) // Moderate",
+            "31 - 40(km/h) // Fresh",
+            "41 - 62(km/h) // Strong",
+            "63 - 87(km/h) // Gale",
+            "88 - 117(km/h) // Storm",
+            ">= 118(km/h) // Hurricane"
+          ],
+          Precipitation: [
+            "Blizzard",
+            "Cloudy",
+            "Fog",
+            "Hail",
+            "Heat Lightning",
+            "Heavy Rain",
+            "Heavy Snow",
+            "Light Rain",
+            "Light Snow",
+            "Partly cloudy",
+            "Rain",
+            "Rainbow",
+            "Sleet",
+            "Snow",
+            "Sunny",
+            "Thundersnow",
+            "Thunderstorm"
+          ],
+          Special: [
+            "Avalanche",
+            "Fairy Circle",
+            "Flood",
+            "Flower Bloom",
+            "Jubilee",
+            "Meteor Shower",
+            "Muggy",
+            "Blight Rain"
+          ]
+        }
+      }
+    }
+  };
+  
