3cd54dcd0713ea715f4a8733ea623d2a60c90f37
Ruutuli <rudhuli@gmail.com>
2025-11-24 00:29:40 -0500

    feat: update vending stock retrieval to include year for accurate data fetching
    - Modified the logic in the getCurrentVendingStockList function to incorporate the year when fetching stock data, ensuring accurate retrieval of stock entries.
    - This change aligns with the recent addition of the year field in the vending stock schema, enhancing data organization and inventory management.

--- database/db.js
+++ database/db.js
@@ -2168,8 +2168,10 @@ const getCurrentVendingStockList = async () => {
  const stockCollection = db.collection("vending_stock");
 
  try {
-  const currentMonth = new Date().getMonth() + 1;
-  const currentStock = await stockCollection.findOne({ month: currentMonth });
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentYear = currentDate.getFullYear();
+  const currentStock = await stockCollection.findOne({ month: currentMonth, year: currentYear });
   if (!currentStock) {
    return null;
   }
